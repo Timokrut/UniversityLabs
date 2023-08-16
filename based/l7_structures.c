054960dc6abfc82ff31c8ee1e897c0266cd2d2bd
Timokrut <potapov.tim14@gmail.com>
2023-08-16 20:06:47 +0300

    structures

---
+++ based/l7_structures.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Car
+{
+    int speed;
+    char name[50];
+    float weight;
+};
+
+
+int main(){
+    struct Car BMW;
+    BMW.speed = 170; 
+    strcpy(BMW.name, "BMW M8");
+    BMW.weight = 2500.00f;
+        
+    struct Car Mercedes = {200, "Mercedes Maybach", 3000.00f};
+    printf("Автомобиль %s обладает скоростью: %d\n", BMW.name, BMW.speed);
+    printf("Автомобиль %s обладает скоростью: %d\n", Mercedes.name, Mercedes.speed);
+    
+
+   return 0; 
+}
