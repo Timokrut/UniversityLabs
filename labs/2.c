c235d7343ceb1691abcc7049d89d291496a32b78
Timokrut <potapov.tim14@gmail.com>
2023-09-20 15:09:00 +0300

    completed lab

--- labs/2.c
+++ labs/2.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 // Ввести строку, вывести только слова, оканчивающиеся на согласную букву.
 
 int main(){
 
     char string[100];
-    char vowel[] = "aeiou";
+    char vowels[] = "aeiou";
     bool flag = true;
-    
+
     while (true)
     {
-        scanf("%s", &string);
-        int l = strlen(string) - 1; // index of last element
-        for (int i = 0; 4; i++)
-        {
-            if (vowel[i] != l)
-            {
-                // add to answer
-                // break;
-            }
-            else
-            {
-                continue;
-            }
-            
+        printf("enter '!' to end program\n");
 
+        // enter a string
+        scanf("%s", string);
+
+        // requirement to end program
+        if (strcmp(string, "!") == 0)
+        break;
+
+        // index of last element
+        int l_idx = strlen(string) - 1; 
+
+        // checking if last symbol is vowel or consonant
+        for (int i = 0; i <= 4; i++)
+        {
+            if (vowels[i] == tolower(string[l_idx]))
+            {
+                flag = false;
+                break;
+            }
         }
         
-
-
+        // if string meets requirements
+        if (flag == true)
+        {
+            printf("%s\n", string);
+        }
+        flag = true;
     }
-    
-    // разделение строки по пробелам
-
-    // опреде
-
-
     return 0;
 }
