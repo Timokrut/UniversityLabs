c5bbb63a2129367180732469adff016a44ab8830
Timofey <potapov.tim14@gmail.com>
2024-02-21 11:55:20 +0300

    new word generator/fills files with random words

--- labs/6/wordsgen.py
+++ labs/6/wordsgen.py
@@ -1,13 +1,17 @@
 from random import randint
 
-number_of_words = randint(50, 100)
-print(number_of_words)
-alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+def generatewords():
+    number_of_words = randint(50, 100)
+    alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+    answer = ''
+    for _ in range(number_of_words):
+        answer += alphabet[randint(0, len(alphabet) - 1)] * randint(1, 15) + ' '
 
-answer = ''
+    print('succesfully filled')
+    
+    return answer
 
-for word in range(number_of_words):
-    answer += alphabet[randint(0, len(alphabet) - 1)] * randint(1, 15) + ' '
-
-
-print(answer)    
+with open('file1.txt', 'w') as first, open('file2.txt', 'w') as second, open('file3.txt', 'w') as third:
+    first.write(generatewords())
+    second.write(generatewords())
+    third.write(generatewords())
