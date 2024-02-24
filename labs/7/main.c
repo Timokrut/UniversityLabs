698b3c1d9193b9f66a8871fc49bc5f181ee91a16
Timokrut <potapov.tim14@gmail.com>
2024-02-25 01:01:45 +0300

    little structure update

--- labs/7/main.c
+++ labs/7/main.c
@@ -2,29 +2,7 @@
 #include <assert.h> 
 #include <string.h>
 #include "function.h"
-
-
-void test_decTo11th()
-{
-    char result[120];
-
-    // Test case 1: 
-    decTo11th(120, result);
-    assert(strcmp(result, "AA") == 0);
-    printf("Test 1 passed\n");
-
-    // Test case 2: 
-    decTo11th(11, result);
-    assert(strcmp(result, "10") == 0);
-    printf("Test 2 passed\n");
-
-    // Test case 3: 
-    decTo11th(0, result);
-    assert(strcmp(result, "0") == 0);
-    printf("Test 3 passed\n");
-
-}
-
+#include "tests.h"
 
 int main()
 {
