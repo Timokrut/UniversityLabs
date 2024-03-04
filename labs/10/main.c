e2f91f5090f9c94917e6e694cb76060d559ec826
Timokrut <potapov.tim14@gmail.com>
2024-03-04 09:50:45 +0300

    base

---
+++ labs/10/main.c
@@ -0,0 +1,12 @@
+int main() {
+    int num1, num2, sum;
+
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
