bf5936cf28b8cb998a194b28958ea40281045471
Timokrut <potapov.tim14@gmail.com>
2024-04-09 11:34:44 +0300

    hw

--- labs/test.c
+++ labs/test.c
@@ -1,57 +1,39 @@
-// вводится строка и номер слова, которое должно перевернуться
-
 #include <stdio.h>
 
-int main() {
-
-    // Ввод строки
-
-    char data[100];
-    printf("enter a string: ");
-
-    int c;
-    int index;
-    while ((c = getchar()) != '\n' && index < 99)
-    {
-        data[index] = c;
-        index ++;
-    }
-    data[index] = '\0';
-
-    // получение индекса слова
-    int idx; 
-    printf("index of word would you like to reverse (from 0): ");
-    scanf("%d", &idx);
-    
-    // ищем слово по индексу
+int count_weight1(unsigned char* number)
+{
+    int i = 0; 
     int counter = 0;
-    int start = 0;
-    int end = 0;
-    for (int i = 0; i < index + 1; i++)
+    while (number[i] != '\0')
     {
-        if (data[i] == ' ' || data[i] == '\0')
-        {   
+        if (number[i] == '1')
             counter++;
-            if (counter == idx)
-                start = i + 1;
-            if (counter - 1 == idx)
-                {
-                    end = i - 1;
-                    break;
-                }    
-        }                
+        i++;    
     }
 
-    // выводим предложение на экран
-    for (int i = 0; i < start; i++)
-        printf("%c", data[i]);
-    
-    for (int i = end; i != start - 1; i--)
-        printf("%c", data[i]);
-    
-    for (int i = end + 1; data[i] != '\0'; i++)
-        printf("%c", data[i]);            
-        
+    return counter;
+}
 
+
+int count_weight2(unsigned char* number)
+{
+    int i = 0; 
+    int counter = 0;
+    while (number[i] != '\0')
+    {
+        counter += number[i] % 2;
+        i++;    
+    }
+
+    return counter;
+}
+
+
+int main()
+{
+    unsigned char binary[9];
+    scanf("%s", binary);
+    printf("%d\n", count_weight1(binary));
+    printf("%d\n", count_weight2(binary));
     return 0;
 }
