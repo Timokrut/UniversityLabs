69bcc761d793d280f82084d2c4190172c7afb22b
Timokrut <potapov.tim14@gmail.com>
2024-05-13 23:50:12 +0300

    completed

--- labs/10/addition/header.h
+++ labs/10/addition/header.h
@@ -1,9 +1,3 @@
 #pragma once
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 void analyzeFile();
-void remove_duplicates();
-
--- labs/10/addition/main.c
+++ labs/10/addition/main.c
@@ -1,11 +1,7 @@
 #include "header.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main() 
 {
     analyzeFile();
-    remove_duplicates();
     return 0;
 }
