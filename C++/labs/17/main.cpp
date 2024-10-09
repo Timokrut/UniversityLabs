88434093307056a82539f99a77f14b5436c93fa8
Timokrut <potapov.tim14@gmail.com>
2024-10-09 15:57:11 +0300

    separated to main and header

--- C++/labs/17/main.cpp
+++ C++/labs/17/main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <cstring> // for memcpy
-#include <ostream>
+#include "header.h"
 
 // insert : DONE (add) 
 // erase : DONE
@@ -10,128 +8,10 @@
 // clear : DONE
 // operator<< - cout : DONE
 
-template <typename T> void bubblesort(T* arr, int size)
-{
-    for (int i = 0; i < size - 1; i++)
-        for (int j = 0; j < size - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-                std::swap(arr[j], arr[j+1]);
-        }
-}
-
-
-template <typename T> class Set 
-{
-    private:
-        T* elements;           
-        int size;                         
-        int capacity;
-
-    public:     
-        Set(int _capacity) : size(0), capacity(_capacity) 
-        {
-            if (_capacity == 0)
-                throw std::runtime_error("Emty Set");
-            elements = new T[capacity];
-        }
-
-        ~Set() 
-        {
-            delete[] elements;
-        }
-
-        Set(const Set& other) : size(other.size), capacity(other.capacity) 
-        {
-            elements = new T[capacity];
-            std::memcpy(elements, other.elements, size * sizeof(T));
-        }
-
-        Set& operator=(const Set& other) 
-        {
-            if (this == &other) 
-                return *this;
-            
-            delete[] elements;
-            size = other.size;
-            capacity = other.capacity;
-            elements = new T[capacity];
-            
-            std::memcpy(elements, other.elements, size * sizeof(T));
-            return *this;
-        }
-
-        void resize(int newCapacity) 
-        {
-            T* newElements = new T[newCapacity];
-            std::memcpy(newElements, elements, size * sizeof(T));
-            delete[] elements;
-            elements = newElements;
-            capacity = newCapacity;
-        }
-
-        void add(T num) 
-        {
-            if (!contains(num)) 
-            {
-                if (size == capacity) 
-                    resize(2 * capacity);
-                elements[size++] = num;
-                bubblesort(elements, size);
-            }
-        }
-
-        void erase(T num) 
-        {
-            for (int i = 0; i < size; i++) 
-                if (elements[i] == num) 
-                {
-                    for (int j = i; j < size - 1; j++) 
-                        elements[j] = elements[j + 1];
-                    size--;
-                    break;
-                }
-        }
-
-        bool contains(T num)  
-        {
-            for (int i = 0; i < size; i++) 
-                if (elements[i] == num) 
-                    return true;
-            return false;
-        }
-
-        int getSize()
-        {
-            return size;
-        }
-
-        void clear()
-        {
-            delete[] elements;
-            elements = new T(capacity);
-            size = 0;
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const Set<T>& set) 
-        {
-            os << "{";
-            for (int i = 0; i < set.size; i++) 
-            {
-                if (i == set.size - 1)
-                    os << set.elements[i];
-                else
-                    os << set.elements[i] << ", ";
-            }
-            os << "}";
-            return os;
-        }
-};
-
 
 int main()
 {
-Set<int> i_set(1);
+    Set<int> i_set(1);
     i_set.add(3);
     i_set.add(2);
     i_set.add(1);
