1cb9735b927ad4c5d1103859e9d96d64e4d47a04
Timokrut <potapov.tim14@gmail.com>
2024-10-15 19:32:07 +0300

    completed 18

--- C++/labs/18/main.cpp
+++ C++/labs/18/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <ostream>
+#include <stdexcept>
 #include <string>
 
 // Реализовать дерево, где у узла может быть несколько детей (т.е. может быть недвоичное дерево). Использовать map<std::string, Node>, т.е. каждый узел в дереве характеризуется строкой. Node хранит список детей.
@@ -12,9 +13,11 @@ class Node{
     public:
         std::string name;
         std::map<std::string, Node> children;
-        Node(std::string& str): name(str) {}
+        
+        Node() = default;
+        Node(const std::string& str): name(str) {}
 
-        friend std::ostream& operator<<(std::ostream os, const Node& node)
+        friend std::ostream& operator<<(std::ostream& os, const Node& node)
         {
             os << node.name << "\n";
             for (const auto& childPair : node.children)
@@ -34,10 +37,24 @@ class Tree{
     public:
         Tree(): root(nullptr) {}
 
-        void addRoot(const std::string& rootName) {}
-        void addNode(const std::string& parent, const std::string nodeName) {}
+        void addRoot(const std::string& rootName) 
+        {
+            root = new Node(rootName);
+        }
 
-        friend std::ostream operator<<(std::ostream& os, const Tree& tree)
+        void addNode(const std::string& parent, const std::string nodeName) 
+        {
+            if (root == nullptr)
+                std::runtime_error("No roots found!");
+
+            Node* parentNode = findNode(*root, parent);
+            if (parentNode)
+                parentNode->children[nodeName] = Node(nodeName);
+            else
+                std::cout << "Error! Node "<< parent << "is not found" << std::endl;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Tree& tree)
         {
             if (tree.root)
                 os << *tree.root;
@@ -47,7 +64,22 @@ class Tree{
         }
 
     private:
-        Node* findNode(Node& currentNode, const std::string& nodeName) {}
+        Node* findNode(Node& currentNode, const std::string& nodeName) 
+        {
+            if (currentNode.name == nodeName)
+                return &currentNode;
+
+            for (auto& childPair : currentNode.children)
+            {
+                Node& childNode = childPair.second;
+                Node* foundNode = findNode(childNode, nodeName);
+                if (foundNode)
+                    return foundNode;
+            }
+            return nullptr;
+        }
+        
+
 };
 
 int main()
