5aab3d241112398c95079938cf8b06034376a686
Timokrut <potapov.tim14@gmail.com>
2024-10-15 20:16:45 +0300

    completed 18

--- C++/labs/18/main.cpp
+++ C++/labs/18/main.cpp
@@ -4,92 +4,96 @@
 #include <stdexcept>
 #include <string>
 
-// Реализовать дерево, где у узла может быть несколько детей (т.е. может быть недвоичное дерево). Использовать map<std::string, Node>, т.е. каждый узел в дереве характеризуется строкой. Node хранит список детей.
-// addRoot(const std::string& rootName)
-// addNode(const std::string& parent,  const std::string& nodeName)
-// operator<< для вывода на экран
+class Node {
+public:
+    std::string name;
+    std::map<std::string, Node> children;
 
-class Node{
-    public:
-        std::string name;
-        std::map<std::string, Node> children;
-        
-        Node() = default;
-        Node(const std::string& str): name(str) {}
+    Node() = default;
+    Node(const std::string& str) : name(str) {}
 
-        friend std::ostream& operator<<(std::ostream& os, const Node& node)
+    friend std::ostream& operator<<(std::ostream& os, const Node& node) 
+    {
+        return node.print(os, 0);
+    }
+
+private:
+    std::ostream& print(std::ostream& os, int indent) const 
+    {
+        for (int i = 0; i < indent; i++) 
         {
-            os << node.name << "\n";
-            for (const auto& childPair : node.children)
-            {
-                const std::string childName = childPair.first;
-                const Node& childNode = childPair.second;
-                os << "--" << childNode;
-            }
-            return os;
+            os << (i == indent - 1 ? "|-- " : "   ");
         }
+
+        os << name << "\n";
+
+        for (const auto& childPair : children) {
+            const Node& childNode = childPair.second;
+            childNode.print(os, indent + 1);
+        }
+        return os;
+    }
 };
 
-class Tree{
-    private:
-        Node* root;
-    
-    public:
-        Tree(): root(nullptr) {}
+class Tree {
+private:
+    Node* root;
 
-        void addRoot(const std::string& rootName) 
-        {
-            root = new Node(rootName);
+public:
+    Tree() : root(nullptr) {}
+
+    void addRoot(const std::string& rootName) 
+    {
+        root = new Node(rootName);
+    }
+
+    void addNode(const std::string& parent, const std::string nodeName) 
+    {
+        if (root == nullptr)
+            throw std::runtime_error("No roots found!");
+
+        Node* parentNode = findNode(*root, parent);
+        if (parentNode)
+            parentNode->children[nodeName] = Node(nodeName);
+        else
+            std::cout << "Error! Node " << parent << " is not found" << std::endl;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Tree& tree) 
+    {
+        if (tree.root)
+            os << *tree.root;
+        else
+            os << "Tree is empty";
+        return os;
+    }
+
+private:
+    Node* findNode(Node& currentNode, const std::string& nodeName) 
+    {
+        if (currentNode.name == nodeName)
+            return &currentNode;
+
+        for (auto& childPair : currentNode.children) {
+            Node& childNode = childPair.second;
+            Node* foundNode = findNode(childNode, nodeName);
+            if (foundNode)
+                return foundNode;
         }
-
-        void addNode(const std::string& parent, const std::string nodeName) 
-        {
-            if (root == nullptr)
-                std::runtime_error("No roots found!");
-
-            Node* parentNode = findNode(*root, parent);
-            if (parentNode)
-                parentNode->children[nodeName] = Node(nodeName);
-            else
-                std::cout << "Error! Node "<< parent << "is not found" << std::endl;
-        }
-
-        friend std::ostream& operator<<(std::ostream& os, const Tree& tree)
-        {
-            if (tree.root)
-                os << *tree.root;
-            else
-                os << "Tree is empty";
-            return os;
-        }
-
-    private:
-        Node* findNode(Node& currentNode, const std::string& nodeName) 
-        {
-            if (currentNode.name == nodeName)
-                return &currentNode;
-
-            for (auto& childPair : currentNode.children)
-            {
-                Node& childNode = childPair.second;
-                Node* foundNode = findNode(childNode, nodeName);
-                if (foundNode)
-                    return foundNode;
-            }
-            return nullptr;
-        }
-        
-
+        return nullptr;
+    }
 };
 
-int main()
+int main() 
 {
     Tree tree;
     tree.addRoot("root");
     tree.addNode("root", "child1");
     tree.addNode("root", "child2");
+    tree.addNode("root", "child3");
     tree.addNode("child1", "child1_1");
     tree.addNode("child1", "child1_2");
+    tree.addNode("child2", "child2_1");
     
     std::cout << tree;
 
