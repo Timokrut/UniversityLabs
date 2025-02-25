8d449a9139f05eed05dec0670e402588510f8f43
Timokrut <potapov.tim14@gmail.com>
2025-02-25 12:51:55 +0300

    second additional

--- Java/2/Main.java
+++ Java/2/Main.java
@@ -11,11 +11,36 @@ public class Main {
         pow.setElement(1, 0, 1);    
         pow.setElement(1, 1, 0);    
 
-        System.out.println(matrix);
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(matrix);
+        //     matrix = matrix.product(pow);
+        // }
 
-        for (int i = 0; i < 10 - 1; i++) {
-            matrix = matrix.product(pow);
-            System.out.println(matrix);
-        }
+        Matrix m1 = new Matrix(3);
+        Matrix m2 = new Matrix(3);
+
+        int counter = 1;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m1.setElement(i, j, counter);
+                counter++;
+            }
+        } 
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                m2.setElement(i, j, counter);
+                counter++;
+            }
+        } 
+    
+        System.out.println(m1);
+        m1.doMiracleDiag(0);
+        System.out.println(m1);
+
+        System.out.println(m2);
+        m2.doMiracleDiag(1);
+        System.out.println(m2);
     }
 }
--- Java/2/Matrix.java
+++ Java/2/Matrix.java
@@ -1,12 +1,14 @@
 
 public class Matrix {
-    public int[][] arr;
+    private int[][] arr;
+    private int size;
 
-    public Matrix(int matrix_size) {
-        arr = new int[matrix_size][matrix_size];
+    public Matrix(int matrixSize) {
+        arr = new int[matrixSize][matrixSize];
+        size = matrixSize;
 
-        for (int row = 0; row < arr.length; row++) {
-            for (int column = 0; column < arr[0].length; column++) {
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
                 if (row == column) {
                     arr[row][column] = 1;
                 }
@@ -15,10 +17,10 @@ public class Matrix {
     }
 
     public Matrix sum(Matrix other) {
-        Matrix result = new Matrix(arr.length);
+        Matrix result = new Matrix(size);
 
-        for (int row = 0; row < arr.length; row++) {
-            for (int column = 0; column < arr[0].length; column++) {
+        for (int row = 0; row < size; row++) {
+            for (int column = 0; column < size; column++) {
                     result.arr[row][column] = arr[row][column] + other.arr[row][column];
             }
         }
@@ -26,14 +28,13 @@ public class Matrix {
         return result;
     }
 
-
     public Matrix product(Matrix other) {
-        Matrix result = new Matrix(arr.length);
+        Matrix result = new Matrix(size);
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 result.arr[i][j] = 0;
-                for (int k = 0; k < arr.length; k++) {
+                for (int k = 0; k < size; k++) {
                     result.arr[i][j] += arr[i][k] * other.arr[k][j]; 
                 }
             }
@@ -53,12 +54,34 @@ public class Matrix {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 result.append(arr[i][j]).append(" ");
             }
             result.append('\n');
         }
         return result.toString();
     } 
+    
+    public void doMiracleDiag(int type) {
+        if (type == 0) {
+            for (int i = 0; i < size; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[j][i];
+                    arr[j][i] = temp;
+                }
+            }
+        } else if (type == 1) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size - i - 1; j++) {
+                    int temp = arr[i][j];
+                    arr[i][j] = arr[size - 1 - j][size - 1 - i];
+                    arr[size - 1 - j][size - 1 - i] = temp;
+                }
+            }
+        } else {
+            System.out.println("Invalid type");
+        }
+    }
 }
