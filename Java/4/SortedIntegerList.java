d6ff2406419ff6c58b7fa440d304564a97b67a2c
Timokrut <potapov.tim14@gmail.com>
2025-03-12 16:27:33 +0300

    start 4

---
+++ Java/4/SortedIntegerList.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class SortedIntegerList { 
+    private LinkedList<Integer> list;
+    private boolean state;
+
+    public SortedIntegerList(boolean bool) {
+        list = new LinkedList<Integer>();
+        this.state = bool;
+    }
+
+    public void add(int number) {
+        if (state) {
+      
+        } else {
+
+        }
+    }
+
+    public void remove(int number) {
+;
+    }
+
+    @Override
+    public final boolean equals(Object obj) {
+        return true;
+    }
+}
