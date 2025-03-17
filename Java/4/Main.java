b48645915ee96a16d2c443afecfad26ddf616524
Timokrut <potapov.tim14@gmail.com>
2025-03-17 23:59:39 +0300

    base 4

--- Java/4/Main.java
+++ Java/4/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        SortedIntegerList list1 = new SortedIntegerList(true); // Разрешены дубликаты
+        list1.add(5);
+        list1.add(3);
+        list1.add(8);
+        list1.add(3);
+        System.out.println("List1: " + list1); // [3, 3, 5, 8]
 
+        SortedIntegerList list2 = new SortedIntegerList(false); // Дубликаты запрещены
+        list2.add(5);
+        list2.add(3);
+        list2.add(8);
+        list2.add(3);
+        System.out.println("List2: " + list2); // [3, 5, 8]
+
+        list1.remove(3);
+        System.out.println("List1 after remove 3: " + list1); // [3, 5, 8]
+
+        System.out.println("List1 equals List2: " + list1.equals(list2)); // true
+
+        list2.remove(8);
+        System.out.println("List2 after remove 8: " + list2); // [3, 5]
+
+        System.out.println("List1 equals List2: " + list1.equals(list2)); // false
     }
 }
--- Java/4/SortedIntegerList.java
+++ Java/4/SortedIntegerList.java
@@ -1,28 +1,59 @@
 import java.util.LinkedList;
+import java.util.Iterator;
 
 public class SortedIntegerList { 
     private LinkedList<Integer> list;
-    private boolean state;
+    private boolean allowDuplicates;
 
-    public SortedIntegerList(boolean bool) {
-        list = new LinkedList<Integer>();
-        this.state = bool;
+    public SortedIntegerList(boolean allowDuplicates) {
+        this.list = new LinkedList<>();
+        this.allowDuplicates = allowDuplicates;
     }
 
-    public void add(int number) {
-        if (state) {
-      
-        } else {
-
+    public void add(int value) {
+        Iterator<Integer> it = list.iterator();
+        int index = 0;
+        
+        while (it.hasNext()) {
+            int current = it.next();
+            if (current > value) {
+                break; 
+            } 
+            if (!allowDuplicates && current == value) {
+                return;
+            }
+            index++;
         }
+
+        list.add(index, value);
     }
 
-    public void remove(int number) {
-;
+    public void remove(int value) {
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            int current = it.next();
+            if (current == value) {
+                it.remove();
+                return;
+            }
+        }
     }
 
     @Override
     public final boolean equals(Object obj) {
-        return true;
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        SortedIntegerList other = (SortedIntegerList) obj;
+        return this.list.equals(other.list) && this.allowDuplicates == other.allowDuplicates;
+    }
+
+    @Override
+    public String toString() {
+        return list.toString();
     }
 }
